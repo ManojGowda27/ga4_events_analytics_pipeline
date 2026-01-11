e6cc978228ebb9531cb54733ccd2d63bc0e4574c
ManojGowda27 <vmanojgowda27@gmail.com>
2026-01-11 17:03:06 -0500

    feat(staging): created flattened events model with unnesting

--- macros/unnest_key.sql
+++ macros/unnest_key.sql
@@ -1,9 +1,14 @@
-{% macro unnest_key(column_name, key_value, value_type='string_value') %}
-    -- This macro searches the 'event_params' array for a specific key and extracts the value associated with it.
+{% macro unnest_key(column_name, key_value) %}
+    -- This macro searches the 'event_params' array for a specific key and extracts the value associated with it as a STRING.
     
     (
         select 
-            coalesce(value.{{ value_type }}, cast(value.int_value as string), cast(value.float_value as string), cast(value.double_value as string))
+            coalesce(
+                value.string_value, 
+                cast(value.int_value as string), 
+                cast(value.float_value as string), 
+                cast(value.double_value as string)
+            )
         from unnest({{ column_name }}) 
         where key = '{{ key_value }}'
     )
